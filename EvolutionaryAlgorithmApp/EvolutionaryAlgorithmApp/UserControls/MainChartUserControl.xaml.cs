f688ae39526afcf8c0793ee9af06f95382486cbb
Korag <koragrex@gmail.com>
2018-05-03 17:21:49 +0200

    Layout v0.5

---
+++ EvolutionaryAlgorithmApp/EvolutionaryAlgorithmApp/UserControls/MainChartUserControl.xaml.cs
@@ -0,0 +1,136 @@
+﻿using LiveCharts;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+using LiveCharts.Wpf.Charts.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeuralNetworkApp.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MainChartUserControl.xaml
+    /// </summary>
+    public partial class MainChartUserControl : UserControl
+    {
+        public static readonly DependencyProperty ChartName =
+        DependencyProperty.Register("MainChartTitle", typeof(String),
+        typeof(MainChartUserControl), new FrameworkPropertyMetadata(string.Empty));
+
+        public String MainChartTitle
+        {
+            get { return GetValue(ChartName).ToString(); }
+            set { SetValue(ChartName, value); }
+
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        private List<double> XValues = new List<double>();
+        private List<double> YValueList;
+            
+        public MainChartUserControl()
+        {
+            InitializeComponent();
+            FillXValues();
+            YFormatter = value => value.ToString();
+        }
+
+
+        private void FillXValues()
+        {
+            for (double i = -1; i < 1; i += 0.1)
+            {
+                XValues.Add(Math.Round(i, 1));
+            }
+        }
+
+        private List<double> FillYValues(double[] Weight)
+        {
+            YValueList = new List<double>();
+            double YValue = 0;
+            if (Weight[1] != 0)
+            {
+                for (int i = 0; i < XValues.Count; i++)
+                {
+                    YValue = -(Weight[0] * XValues[i] - Weight[2]) / (Weight[1]);
+                    YValueList.Add(YValue);
+                }
+            }
+            else
+            {
+                FillListWithZeros(YValueList);
+            }
+            return YValueList;
+        }
+
+        private void FillListWithZeros(List<double> TempList)
+        {
+            for (int i = 0; i < XValues.Count; i++)
+            {
+
+                TempList.Add(0);
+            }
+        }
+
+        private SeriesCollection FillMainChartSeriesWithYValues(int NumberOfWeights, List<double[]> Weights)
+        {
+            List<double> YValues = null;
+            SeriesCollection seriesCollection = new SeriesCollection();
+            int FunctionNumber = 0;
+            while (FunctionNumber < NumberOfWeights)
+            {
+                YValues = FillYValues(Weights[FunctionNumber]);
+
+
+                seriesCollection.Add
+                (
+                    new LineSeries
+                    {
+                        Title = $"Function {FunctionNumber + 1}",
+                        Values = YValues.AsChartValues(),
+                        Stroke = new SolidColorBrush(Chart.Colors[FunctionNumber])
+
+                    }
+                );
+
+                FunctionNumber++;
+            }
+
+            return seriesCollection;
+        }
+
+        public void DrawChart(int NumberOfWeights, List<double[]> Weights)
+        {
+            SeriesCollection = FillMainChartSeriesWithYValues(NumberOfWeights, Weights);
+
+            Labels = ConvertFromDoubleToString();
+            DataContext = this;
+        }
+
+        private string[] ConvertFromDoubleToString()
+        {
+            string[] TempArray = new string[XValues.Count];
+
+            for (int i = 0; i < XValues.Count; i++)
+            {
+                TempArray[i] = XValues[i].ToString();
+            }
+            return TempArray;
+        }
+        
+    }
+}
---
+++ EvolutionaryAlgorithmApp/EvolutionaryAlgorithmApp/UserControls/pointValueUserControl.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeuralNetworkApp.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for pointValueUserControl.xaml
+    /// </summary>
+    public partial class pointValueUserControl : UserControl
+    {
+        public static readonly DependencyProperty TextProperty1 =
+        DependencyProperty.Register("TextValue", typeof(String),
+        typeof(pointValueUserControl), new FrameworkPropertyMetadata(string.Empty));
+
+        public String TextValue
+        {
+            get { return GetValue(TextProperty1).ToString(); }
+            set { SetValue(TextProperty1, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty2 =
+        DependencyProperty.Register("FirstValueText", typeof(String),
+        typeof(pointValueUserControl), new FrameworkPropertyMetadata("0"));
+        public String FirstValueText
+        {
+            get { return GetValue(TextProperty2).ToString(); }
+            set { SetValue(TextProperty2, value); }
+
+        }
+
+        public static readonly DependencyProperty TextProperty3 =
+        DependencyProperty.Register("SecondValueText", typeof(String),
+        typeof(pointValueUserControl), new FrameworkPropertyMetadata("0"));
+        public String SecondValueText
+        {
+            get { return GetValue(TextProperty3).ToString(); }
+            set { SetValue(TextProperty3, value); }
+        }
+        public static readonly DependencyProperty TextProperty4 =
+       DependencyProperty.Register("ThirdValueText", typeof(String),
+       typeof(pointValueUserControl), new FrameworkPropertyMetadata("-1"));
+        public String ThirdValueText
+        {
+            get { return GetValue(TextProperty4).ToString(); }
+            set { SetValue(TextProperty4, value); }
+        }
+
+
+        public static readonly RoutedEvent TextChangedEvent =
+       EventManager.RegisterRoutedEvent("TextChanged", RoutingStrategy.Bubble,
+       typeof(RoutedEventHandler), typeof(pointValueUserControl));
+
+        public event RoutedEventHandler TextChanged
+        {
+            add { AddHandler(TextChangedEvent, value); }
+            remove { RemoveHandler(TextChangedEvent, value); }
+        }
+        private void OnTextChanged(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(TextChangedEvent));
+        }
+
+
+        public pointValueUserControl()
+        {
+            InitializeComponent();
+        }
+       
+
+    }
+}
